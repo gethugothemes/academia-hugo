7392d291a96d7f86d3558375c2f3a5dae9fc7cd0
Roy Chan <d0uuub@gmail.com>
2020-03-29 17:17:51 +0800

    search fix

--- assets/js/academia.js
+++ assets/js/academia.js
@@ -71,7 +71,7 @@
   $('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $('[name="' + this.hash.slice(1) + '"]');
       if (target.length) {
         html_body.animate({
           scrollTop: target.offset().top - navbar_offset
